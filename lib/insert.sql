7ee0e31d7f09570812023d7b6733525188d629f7
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-24 03:15:57 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,5 @@
 INSERT INTO users (id, name, age) VALUES (1,"Daniel", 43), (2,"Ariana", 14), (3,"Mina", 44), (4,"Mikel", 19), (5,"Belinda", 48), (6,"Gloria", 62), (7,"George", 59), (8,"Archie", 54), (9,"Felipe", 88), (10,"Aurora", 85), (11,"Nora", 53), (12,"Bob", 28), (13,"Jude", 33), (14,"Billy", 42), (15,"Maria", 50), (16,"John", 18), (17,"Amy", 34), (18,"Billy", 17), (19,"Joseph", 69), (20,"Abigail", 28);
 
-
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1,"school", 1000000, "January 1, 2018", "December 31, 2018"), (2,"clubhouse", 1000, "January 10, 2018", "October 31, 2018"), (3,"pool", 25000, "March 1, 2018", "August 31, 2018"), (4,"car", 5000, "June 20, 2018", "June 30, 2018"), (5,"pet_shelter", 50000, "January 1, 2018", "December 31, 2018"), (6,"website", 15000, "February 18, 2018", "July 2, 2018"), (7,"computer", 800, "September 1, 2018", "September 15, 2018"), (8,"garden", 350, "July 1, 2018", "July 31, 2018"), (9,"wall", 7500, "January 1, 2018", "July 31, 2018"), (10,"art_wall", 1500, "June 22, 2018", "July 18, 2018");
 
 
